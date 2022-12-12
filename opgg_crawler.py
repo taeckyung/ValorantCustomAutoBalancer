904a3c733db0537d4dbcb193d36e48e37c74bb48
Taeckyung Lee <taeckyung@nmsl.kaist.ac.kr>
2022-12-12 16:24:06 +0900

    add validations

--- opgg_crawler.py
+++ opgg_crawler.py
@@ -1,4 +1,5 @@
 
+# %%
 import json
 import time
 import requests
@@ -9,33 +10,41 @@ from pprint import pprint
 from datetime import datetime, timedelta
 
 
-MAX_SEARCH_LIMIT = 100  # must be the multiple of 20
+# %%
+MAX_SEARCH_PAGE_LIMIT = 100  # must be the multiple of 20
+MAX_SEARCH_DAY_LIMIT = 30  # days
 HEADER = {'Accept-Language':'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'}
 
 
 user_df = pd.read_csv("account.csv", encoding="utf-8")
 print(user_df)
 
-match_dict: Dict[str, Dict[str, str]] = dict()
-user_record_df_dict: Dict[str, Dict] = dict()
+match_candidates_to_search: Dict[str, Dict[str, str]] = dict()
+user_stats_list: Dict[str, Dict] = dict()
 
+
+# %%
+# refresh records
+for _, row in user_df.iterrows():
+    name = row["gameName"].upper()
+    tag = row["tagLine"].upper()
+
+    user_name_merged = f"{name}#{tag}"
+    if user_name_merged not in user_stats_list:
+        user_stats_list[user_name_merged] = []
+
+    url = f"https://valorant.op.gg/api/renew?gameName={name}&tagLine={tag}"
+    requests.get(url,headers=HEADER)
+
+
+# %%
+# get match records
 for _, row in user_df.iterrows():
     name = row["gameName"].upper()
     tag = row["tagLine"].upper()
     print(name, tag)
 
-    user_name_merged = f"{name}#{tag}"
-    if user_name_merged not in user_record_df_dict:
-        user_record_df_dict[user_name_merged] = []
-
-    # refresh records
-    url = f"https://valorant.op.gg/api/renew?gameName={name}&tagLine={tag}"
-    requests.get(url,headers=HEADER)
-
-    # time.sleep(1.0)
-
-    for offset in range(0, MAX_SEARCH_LIMIT+1, 20):
-        # match records
+    for offset in range(0, MAX_SEARCH_PAGE_LIMIT+1, 20):
         url = f"https://valorant.op.gg/api/player/matches?gameName={name}&tagLine={tag}&queueId=custom&offset={offset}&limit=20"
         response = requests.get(url,headers=HEADER)
         match_record = json.loads(response.text)
@@ -45,32 +54,41 @@ for _, row in user_df.iterrows():
             continue
 
         for match in match_record:
-            if match['queueId'] == '':  # custom game does not have queueid
-                if match["roundResults"] is not None:
-                    round_result = str(match["roundResults"])
-                    left = int(round_result.split(":")[0])
-                    right = int(round_result.split(":")[1])
-                    if left > 12 or right > 12:  # properly ended games
-                        match_time = datetime.strptime(match["gameStartDateTime"], "%Y-%m-%dT%H:%M:%S.%fZ")
-                        if match_time > datetime.now() - timedelta(days=30):  # only include within 30 days
-                            match_dict[match['matchId']] = {
-                                'name': name,
-                                'tag': tag,
-                                'gameStartDateTime': match_time
-                            }
+            if match['queueId'] != '':  # custom game does not have queueid
+                continue
 
+            if match["roundResults"] is None:  # invalid custom game
+                continue
 
+            if match["matchId"] in match_candidates_to_search:  # already exists
+                continue
 
+            round_result = str(match["roundResults"])
+            win_count_l = int(round_result.split(":")[0])
+            win_count_r = int(round_result.split(":")[1])
+
+            if win_count_l != 13 and win_count_r != 13:  # properly ended games / exclude deathmatches
+                continue
+
+            match_time = datetime.strptime(match["gameStartDateTime"], "%Y-%m-%dT%H:%M:%S.%fZ")
+
+            # only include within MAX_SEARCH_DAY_LIMIT days
+            if match_time > datetime.now() - timedelta(days=MAX_SEARCH_DAY_LIMIT):  
+                match_candidates_to_search[match['matchId']] = {
+                    'name': name,
+                    'tag': tag,
+                    'gameStartDateTime': match_time
+                }
 
 # with open('data.json', 'w') as f:
 #     json.dump(match_record_list, f)
 
 
-match_record_list = []
+# %%
 
-for key in match_dict.keys():
+for key in match_candidates_to_search.keys():
     match_id = key
-    val = match_dict[key]
+    val = match_candidates_to_search[key]
     name = val['name'].upper()
     tag = val['tag'].upper()
     
@@ -78,7 +96,9 @@ for key in match_dict.keys():
     response = requests.get(url,headers=HEADER)
     match_record = json.loads(response.text)
 
-    match_record_list.append(match_record)
+    # validation
+    if len(match_record["participants"]) != 10:
+        continue
 
     for user in match_record["participants"]:
         if user["riotAccount"] is None:
@@ -88,11 +108,11 @@ for key in match_dict.keys():
         user_tag = user["riotAccount"]["tagLine"].upper()
         user_name_merged = f"{user_name}#{user_tag}"
 
-        if user_name_merged not in user_record_df_dict:
+        if user_name_merged not in user_stats_list:
             print(user_name_merged)
             continue
 
-        user_record_df_dict[user_name_merged].append(
+        user_stats_list[user_name_merged].append(
             {
                 "win": float(user["won"]),
                 "score": float(user["scorePerRound"]),
@@ -101,11 +121,10 @@ for key in match_dict.keys():
         )
 
 
-for user_name in user_record_df_dict.keys():
-    df = pd.DataFrame.from_records(user_record_df_dict[user_name], columns=["win", "score", "ranking"])
+# %%
+for user_name in user_stats_list.keys():
+    df = pd.DataFrame.from_records(user_stats_list[user_name], columns=["win", "score", "ranking"])
     print("--------------------------------------")
     print(user_name)
     pprint(df.mean(axis=0))
     print("--------------------------------------")
-
-# pprint(user_record_df_dict)

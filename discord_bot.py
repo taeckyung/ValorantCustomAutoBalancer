6c7d7e38e08a406568e662e78f0e8034b24d793f
Taeckyung Lee <taeckyung@nmsl.kaist.ac.kr>
2022-12-14 02:29:39 +0900

    add autobalancing

--- discord_bot.py
+++ discord_bot.py
@@ -1,17 +1,42 @@
-from opgg_crawler import get_stats
+import opgg_crawler
 import discord
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         print(f'Message from {message.author}: {message.content}')
-        if message.content.startswith('!스탯'):
-            pending_message = await message.channel.send("10초정도 기다리세용")
-            stats = await get_stats()
-            await pending_message.delete()
-            await message.channel.send(content=stats)
+        content = message.content
+        if content.startswith('!내전'):
+            content = content.replace('!내전','').strip()
+            if content.startswith('스탯'):
+                pending_message = await message.channel.send("10초정도 기다리세용")
+                total_match_cnt, latest_match_time, _ = await opgg_crawler.get_stats()
+                string = f"Total Matches: {total_match_cnt} \t|\t Latest Match: {latest_match_time.isoformat()} \t|\t rating := sqrt(win) * score\n"
+                await pending_message.delete()
+                await message.channel.send(content=string, file=discord.File("table.png"))
+            elif content.startswith('멤버'):
+                content = content.replace('멤버','').strip()
+                if content.startswith('추가'):
+                    content = content.replace('추가','').strip()
+                    result = await opgg_crawler.add_member(content)
+                    await message.channel.send(content=result)
+                elif content.startswith('삭제'):
+                    content = content.replace('삭제','').strip()
+                    result = await opgg_crawler.remove_member(content)
+                    await message.channel.send(content=result)
+                else:
+                    result = await opgg_crawler.get_member()
+                    await message.channel.send(content=result)
+            elif content.startswith('자동밸런스') or content.startswith('자밸'):
+                pending_message = await message.channel.send("10초정도 기다리세용")
+                result = await opgg_crawler.auto_balance()
+                await pending_message.delete()
+                await message.channel.send(content=result)
+            else:
+                await message.channel.send(content="사용법: `!내전 멤버` `!내전 멤버 추가 [이름]` `!내전 멤버 삭제 [이름]` `!내전 자동밸런스` `!내전 스탯`")
+
 
 intents = discord.Intents.default()
 intents.message_content = True
